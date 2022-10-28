0018eed1a222e799d843a6635479e0508dcc38fa
ahorner1117 <61562591+ahorner1117@users.noreply.github.com>
2022-10-28 11:56:57 -0400

    Additional skills

---
+++ src/components/BackToTop.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class BackToTop extends Component {
+  render() {
+    return (
+      <section id="backToTop">
+        <div className="back-to-top"></div>
+      </section>
+    );
+  }
+}
+
+export default BackToTop;
