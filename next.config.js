3e471c0384575821d57432d6cfd7c6f838c11d96
ahorner1117 <61562591+ahorner1117@users.noreply.github.com>
2023-12-20 12:00:13 -0500

    Update next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	images: { unoptimized: true },
 	reactStrictMode: true,
 	swcMinify: true,
-	output: 'export',
+	output: 'standalone',
 
 	webpack(config) {
 		config.module.rules.push({
