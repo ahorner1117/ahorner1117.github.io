5fc29d6585e1ff27092db32344e8acbc1619d8d0
ahorner1117 <61562591+ahorner1117@users.noreply.github.com>
2023-12-21 14:40:18 -0500

    Change google tag

--- app/layout.js
+++ app/layout.js
@@ -7,71 +7,49 @@ import { ThemeContext } from "context";
 export const metadata = { ...AppMetadata };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<head>
-				{/* Existing stylesheets */}
-				{/* Google Analytics */}
-				<script async src="https://www.googletagmanager.com/gtag/js?id=G-JQMY74LT90"></script>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-JQMY74LT90');
-          `
-					}}
-				></script>
-				<link
-					rel="stylesheet"
-					href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
-				/>
+  return (
+    <html lang="en">
+      <head>
+        {/* Existing stylesheets */}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
 
-				{/* Google Tag Manager */}
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5V27ML56');`
-					}}
-				></script>
-				{/* End Google Tag Manager */}
+          })(window,document,'script','dataLayer','GTM-5V27ML56');`,
+        }}></script>
+        {/* End Google Tag Manager */}
 
-				{/* Google Analytics */}
-				<script async src="https://www.googletagmanager.com/gtag/js?id=G-JQMY74LT90"></script>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6KR6QQZ2PH"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-JQMY74LT90');
-          `
-					}}
-				></script>
-				{/* End Google Analytics */}
-			</head>
-			<body>
-				{/* Google Tag Manager (noscript) */}
-				<noscript>
-					<iframe
-						src="https://www.googletagmanager.com/ns.html?id=GTM-5V27ML56"
-						height="0"
-						width="0"
-						style={{ display: "none", visibility: "hidden" }}
-					></iframe>
-				</noscript>
-				{/* End Google Tag Manager (noscript) */}
+            gtag('config', 'G-6KR6QQZ2PH');
+          `,
+        }}></script>
+        {/* End Google Analytics */}
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5V27ML56"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}>
+          </iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
 
-				<ThemeContext>
-					<AppHeader />
-					<Suspense fallback={<Loading />}>{children}</Suspense>
-					<AppFooter />
-				</ThemeContext>
-			</body>
-		</html>
-	);
+        <ThemeContext>
+          <AppHeader />
+          <Suspense fallback={<Loading />}>{children}</Suspense>
+          <AppFooter />
+        </ThemeContext>
+      </body>
+    </html>
+  );
 }
