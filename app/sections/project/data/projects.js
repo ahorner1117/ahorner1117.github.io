db4faf3758afe6902274d65f783adfdcfe7225eb
ahorner1117 <61562591+ahorner1117@users.noreply.github.com>
2023-12-21 13:07:37 -0500

    Add modal and truncate words on projects section

---
+++ app/sections/project/data/projects.js
@@ -0,0 +1,112 @@
+export const ecommerce = [
+	{
+		title: "Official Store of Madison Square Garden",
+		link: "https://shop.msg.com/",
+		description:
+			"In my role as a developer for the MSG store, I took on the responsibility of developing product landing pages, search and catalog pages. Additionally, I led the development of a Shopify checkout app, enabling Shopify Admins to personalize the checkout banner.",
+		imageUrl:
+			"https://shop.msg.com/cdn/shop/files/de2060ed5c8abac97892b9f195050dce_1.png?v=1696632348&width=600",
+		caseStudyUrl: "#",
+		tools: {
+			"devicon-javascript-plain": "Javascript",
+			"devicon-react-original": "React",
+			"devicon-css3-plain-wordmark": "CSS",
+			"devicon-graphql-plain": "GraphQL",
+			FaShopify: "Shopify Plus",
+			"devicon-github-original": "Github"
+		}
+	},
+	{
+		title: "FIFA Store",
+		link: "https://store.fifa.com/",
+		description:
+			"As a developer on the US Soccer store, my responsibilities included converting Figma files into Shopify webpages. I designed pages with a focus on empowering Shopify admins to easily update content without the need for code changes, utilizing the customizer for seamless modifications. Additionally, I engineered an employee discount app using React and leveraged checkout extensibility. For this application, I seamlessly integrated with Shopify's Admin API and Storefront API, ensuring efficient and effective functionality.",
+		imageUrl:
+			"https://th.bing.com/th/id/R.e062b1838b2bd59f7144d9df03afc237?rik=CmLRmQ1FKzk8Jg&pid=ImgRaw&r=0",
+		caseStudyUrl: "",
+		tools: {
+			"devicon-nextjs-original": "Next.js",
+			"devicon-graphql-plain": "GraphQL",
+			FaShopify: "Shopify Plus",
+			"devicon-github-original": "Github",
+			"devicon-tailwindcss-plain": "TailwindCSS"
+		}
+	},
+	{
+		title: "US Soccer",
+		link: "https://store.ussoccer.com/",
+		description:
+			"As a developer on the US Soccer store, my responsibilities included converting Figma files into Shopify webpages. I designed pages with a focus on empowering Shopify admins to easily update content without the need for code changes, utilizing the customizer for seamless modifications. Additionally, I engineered an employee discount app using React and leveraged checkout extensibility. For this application, I seamlessly integrated with Shopify's Admin API and Storefront API, ensuring efficient and effective functionality.",
+		imageUrl: "https://th.bing.com/th/id/OIP.c9k4HQs8k_7zregFn0PesAHaJX?rs=1&pid=ImgDetMain",
+		caseStudyUrl: "#",
+		tools: {
+			"devicon-javascript-plain": "Javascript",
+			"devicon-react-original": "React",
+			"devicon-tailwindcss-plain": "TailwindCSS",
+			"devicon-graphql-plain": "GraphQL",
+			FaShopify: "Shopify Plus",
+			"devicon-github-original": "Github"
+		}
+	},
+	{
+		title: "Century 21",
+		link: "https://www.c21stores.com/",
+		description:
+			"Century 21, a renowned retail brand, boasts a prominent three-story brick and mortar store located in the heart of New York City. During my time at Legends, a collaborative team consisting of myself and two fellow developers successfully spearheaded the development of a headless Shopify store utilizing Next.js framework.",
+		imageUrl:
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKoHoVcPTVKSACUzq321fhqXyX_xAOvBFiLrB26hRifIsHjM4RIHfCnsVFvby_UE13WI0&usqp=CAU",
+		caseStudyUrl: "#",
+		tools: {
+			"devicon-nextjs-original": "Next.js",
+			"devicon-tailwindcss-plain": "TailwindCSS",
+			"devicon-graphql-plain": "GraphQL",
+			FaShopify: "Shopify Plus",
+			"devicon-github-original": "Github"
+		}
+	},
+	{
+		title: "Redcon1",
+		link: "https://redcon1.com/",
+		description:
+			"In my capacity as a software developer with Redcon1, I played a key role in transforming a Shopify Plus store into a headless Shopify store built on Next.js. Facing the constraints of Shopify, the company opted for a headless architecture using Next.js, Contentful as a CMS, and leveraging Nacelle and Vercel. Collaborating with a team of three other developers, I contributed to the creation of the store by translating Figma designs into meticulously crafted web pages.",
+		imageUrl: "https://athletes.redcon1.com/wp-content/uploads/sites/2/2021/03/REDCON1.png",
+		caseStudyUrl: "#",
+		tools: {
+			"devicon-nextjs-original": "Next.js",
+			"devicon-graphql-plain": "GraphQL",
+			"devicon-sass-original": "SASS",
+			FaShopify: "Shopify Plus",
+			"devicon-github-original": "Github",
+			"devicon-jquery-plain": "jQuery"
+		}
+	}
+];
+export const projects = [
+	{
+		title: "Moving Mountains Recovery",
+		link: "https://movingmountainsrecovery.com/",
+		description:
+			"As a freelance developer collaborating with Moving Mountains Recovery, I specialize in crafting customized landing pages. My work involves utilizing PHP and CSS to ensure seamless integration and customization through the WordPress admin interface.",
+		imageUrl:
+			"https://movingmountainsrecovery.com/wp-content/uploads/2021/05/Logo-for-Website-01.png",
+		caseStudyUrl: "#",
+		tools: {
+			FaWordpress: "Wordpress",
+			"devicon-php-plain": "PHP",
+			"devicon-github-original": "Github"
+		}
+	},
+	{
+		title: "Simply Listed Marketing",
+		link: "https://simplylistedmarketing.com/",
+		description:
+			"In my role as a freelance developer for Simply Listed Marketing's website, I took on the responsibility of crafting captivating landing pages and revamping existing web content. Leveraging my skills in PHP, Elementor, CSS, and various WordPress plugins, I contributed to the creation of aesthetically pleasing web pages. Furthermore, I executed the migration of the WordPress website, seamlessly transferring it from one domain and hosting provider to another.",
+		imageUrl: "https://simplylistedmarketing.com/wp-content/uploads/2023/10/Original-Logo-1.png",
+		caseStudyUrl: "#",
+		tools: {
+			FaWordpress: "Wordpress",
+			"devicon-php-plain": "PHP",
+			"devicon-github-original": "Github"
+		}
+	}
+];
--- app/sections/timeline/timeline.jsx
+++ app/sections/timeline/timeline.jsx
@@ -1,109 +1,131 @@
-import React from 'react';
+import { domAnimation, LazyMotion, useInView } from "framer-motion";
+import React, { useRef } from "react";
 
-const experiences= [
-    {
-      "company": "Legends",
-      "id": 1,
-      "role": "Ecommerce Developer",
-      "date": "11.2022 - Present",
-      "description": [
-        "Spearheaded upgrades for multiple Shopify stores, introducing checkout extensibility for enhanced functionality.",
-        "Developed versatile Shopify checkout extensibility apps, including one leveraging React for universal employee discounts and another displaying pickup times and locations in checkout, streamlining the selection process for users.",
-        "Continued to enhance the customizable, headless Next.js and React E-commerce store on the Shopify platform, ensuring adaptability to emerging technologies and evolving business needs.",
-        "Employed GraphQL for API calls, optimizing data retrieval and interaction with Shopify's Admin API and Storefront API.",
-        "Implemented custom use hooks and utilized Redux for efficient state management, ensuring a robust architecture for the E-commerce platform.",
-        "Integrated Contentful headless CMS seamlessly using GraphQL queries and mutations for dynamic data access.",
-        "Translated Figma and Photoshop design files into responsive web interfaces, prioritizing a visually appealing and user-friendly experience across devices."
-      ]
-    },
-    {
-      "company": "Preddio Technologies",
-      "role": "Application Engineer",
-      "id": "2",
+const experiences = [
+	{
+		company: "Legends",
+		id: 1,
+		role: "Ecommerce Developer",
+		date: "11.2022 - Present",
+		description: [
+			"Spearheaded upgrades for multiple Shopify stores, introducing checkout extensibility for enhanced functionality.",
+			"Developed versatile Shopify checkout extensibility apps, including one leveraging React for universal employee discounts and another displaying pickup times and locations in checkout, streamlining the selection process for users.",
+			"Continued to enhance the customizable, headless Next.js and React E-commerce store on the Shopify platform, ensuring adaptability to emerging technologies and evolving business needs.",
+			"Employed GraphQL for API calls, optimizing data retrieval and interaction with Shopify's Admin API and Storefront API.",
+			"Implemented custom use hooks and utilized Redux for efficient state management, ensuring a robust architecture for the E-commerce platform.",
+			"Integrated Contentful headless CMS seamlessly using GraphQL queries and mutations for dynamic data access.",
+			"Translated Figma and Photoshop design files into responsive web interfaces, prioritizing a visually appealing and user-friendly experience across devices."
+		]
+	},
+	{
+		company: "Preddio Technologies",
+		role: "Application Engineer",
+		id: "2",
 
-      "date": "06.2022 - 11.2022",
-      "description": [
-        "Developed API routes using Node.js and Express to transfer data packets gathered from bluetooth gateways to their corresponding cloud portal.",
-        "Developed server-side logic, including data storage and optimizing website performance.",
-        "Developed dynamic user-role based components focused on security and user permissions."
-      ]
-    },
-    {
-      "company": "Redcon1",
-      "id": 3,
+		date: "06.2022 - 11.2022",
+		description: [
+			"Developed API routes using Node.js and Express to transfer data packets gathered from bluetooth gateways to their corresponding cloud portal.",
+			"Developed server-side logic, including data storage and optimizing website performance.",
+			"Developed dynamic user-role based components focused on security and user permissions."
+		]
+	},
+	{
+		company: "Redcon1",
+		id: 3,
 
-      "role": "Front End Developer",
-      "date": "03.2021 - 06.2022",
-      "description": [
-        "Using Javascript, created QR code with augmented reality to provide product information. Inside Walmart, customers can scan corresponding QR code and point their camera at any product to see an informative video explaining the products objective.",
-        "Implemented Shopify buy button using Javascript and jQuery to bind shopping carts across multiple domains using Shopify and Wordpress",
-        "Setup affiliate program to reimburse company brand ambassadors programtically based on their amount of sales each month.",
-        "Implemented Algolia third party search functionality through a customized build, added search filters based on customer type, built product and collection filters for search page."
-      ]
-    },
-    {
-      "company": "Insurance Express",
-      "id": 4,
+		role: "Front End Developer",
+		date: "03.2021 - 06.2022",
+		description: [
+			"Using Javascript, created QR code with augmented reality to provide product information. Inside Walmart, customers can scan corresponding QR code and point their camera at any product to see an informative video explaining the products objective.",
+			"Implemented Shopify buy button using Javascript and jQuery to bind shopping carts across multiple domains using Shopify and Wordpress",
+			"Setup affiliate program to reimburse company brand ambassadors programtically based on their amount of sales each month.",
+			"Implemented Algolia third party search functionality through a customized build, added search filters based on customer type, built product and collection filters for search page."
+		]
+	},
+	{
+		company: "Insurance Express",
+		id: 4,
 
-      "role": "Software Developer",
-      "date": "11.2020 - 03.2021",
-      "description": [
-        "Developed Java application with maven using selenium to parse CSV files containing call records from our softphone, used that data to make REST API calls to a CRM to update the deal stage based on the disposition of the call.",
-        "Created a Java web application to make API requests to update hundreds of thousands of customer records and cancel insurance policies if customer did not pay their premium, stored updates in MySQL database. The front end GUI contained charts from google charts API to display customers policy cancellations by month.",
-        "Created custom email signatures using HTML and CSS for entire company."
-      ]
-    },
-    {
-      "company": "Anju Software",
-      "role": "Clinical Development Intern",
-      "date": "07.2019 - 11.2020",
-      "id": 5,
-      "mainTech": ["T-SQL", "fa fa-database"],
-      "description": [
-        "Designed, developed and maintained clinical trial components from initial build throughout the entire study build project lifecycle.",
-        "Built new clinical trial database components based on the interpretation of study build requirements within assigned timelines.",
-        "Restored databases and analyzed data discrepancies between the UI and the database using SQL scripts in SSMS and excel."
-      ]
-    }
-  ]
+		role: "Software Developer",
+		date: "11.2020 - 03.2021",
+		description: [
+			"Developed Java application with maven using selenium to parse CSV files containing call records from our softphone, used that data to make REST API calls to a CRM to update the deal stage based on the disposition of the call.",
+			"Created a Java web application to make API requests to update hundreds of thousands of customer records and cancel insurance policies if customer did not pay their premium, stored updates in MySQL database. The front end GUI contained charts from google charts API to display customers policy cancellations by month.",
+			"Created custom email signatures using HTML and CSS for entire company."
+		]
+	},
+	{
+		company: "Anju Software",
+		role: "Clinical Development Intern",
+		date: "07.2019 - 11.2020",
+		id: 5,
+		mainTech: ["T-SQL", "fa fa-database"],
+		description: [
+			"Designed, developed and maintained clinical trial components from initial build throughout the entire study build project lifecycle.",
+			"Built new clinical trial database components based on the interpretation of study build requirements within assigned timelines.",
+			"Restored databases and analyzed data discrepancies between the UI and the database using SQL scripts in SSMS and excel."
+		]
+	}
+];
 
-  const TimelineItem = ({ experience, side }) => {
-    // Split the description into an array of bullet points
-    const descriptionList = experience.description.map((point, index) => (
-      <li key={index} className="mb-1">{point}</li>
-    ));
-  
-    return (
-      <div className={`relative text-sm w-full my-6 ${side === 'right' ? 'md:ml-auto' : 'md:mr-auto'} md:w-5/12 ${side === 'right' ? 'bg-black' : 'bg-black'} border border-gray-600 rounded-lg p-6 text-white`}>
-      <h3 className="text-xl font-semibold mb-2">{experience.role}</h3>
-      <h4 className="text-purple-300 mb-4">{experience.company}</h4>
-      <p className="text-gray-400 mb-2">{experience.date}</p>
-      {/* Display bulleted list for the description */}
-      <ul className="list-disc text-gray-300 pl-6">
-        {descriptionList}
-      </ul>
-    </div>
-    );
-  };
+const TimelineItem = ({ experience, side }) => {
+  const ref = useRef(null);
 
-   const VerticalTimeline = () =>{
-    return (
-      <div className="h-auto text-white py-12 bg-transparent">
-        <h2 className="text-4xl font-bold mb-16 text-center">Work Experience</h2>
-        <div className="container mx-auto px-6">
-          <div className="relative w-full">
-            <div className="hidden md:block border-l-2 border-purple-600 border-purple-450 absolute left-1/2 transform -translate-x-1/2 h-full"></div>
-            {experiences.map((experience, index) => (
-              <TimelineItem
-                key={experience.id}
-                experience={experience}
-                side={index % 2 === 0 ? 'left' : 'right'}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-    );
-  };
+	const isItemInView = useInView(ref, { once: true });
 
-  export default VerticalTimeline;
+	const descriptionList = experience.description.map((point, index) => (
+		<li key={index} className="mb-1">
+			{point}
+		</li>
+	));
+
+
+	return (
+		<LazyMotion features={domAnimation}>
+			<div
+				ref={ref}
+				style={{
+					transform: side === 'right' && isItemInView
+          ? 'translateX(-50px)'
+          : side === 'left' && isItemInView
+          ? 'translateX(50px)'
+          : 'none',
+					opacity: isItemInView ? 1 : 0,
+					transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+				}}
+				className={`relative text-sm w-full my-6 ${
+					side === "right" ? "md:ml-auto" : "md:mr-auto"
+				} md:w-5/12 ${
+					side === "right" ? "bg-black" : "bg-black"
+				} border border-gray-600 rounded-lg p-6 text-white`}
+			>
+				<h3 className="text-xl font-semibold mb-2">{experience.role}</h3>
+				<h4 className="text-purple-300 mb-4">{experience.company}</h4>
+				<p className="text-gray-400 mb-2">{experience.date}</p>
+				<ul className="list-disc text-gray-300 pl-6">{descriptionList}</ul>
+			</div>
+		</LazyMotion>
+	);
+};
+
+const VerticalTimeline = () => {
+	return (
+		<div className="h-auto text-white py-12 bg-transparent">
+			<h2 className="text-4xl font-bold mb-16 text-center">Work Experience</h2>
+			<div className="container mx-auto px-6">
+				<div className="relative w-full">
+					<div className="hidden md:block border-l-2 border-purple-600 border-purple-450 absolute left-1/2 transform -translate-x-1/2 h-full"></div>
+					{experiences.map((experience, index) => (
+						<TimelineItem
+							key={experience.id}
+							experience={experience}
+							side={index % 2 === 0 ? "left" : "right"}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default VerticalTimeline;
